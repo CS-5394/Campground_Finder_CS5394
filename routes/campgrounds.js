6c3c62fa0b88d2c0d05832b877a9a83b51826400
archanagogi31 <archanagogi@gmail.com>
2020-04-04 16:03:42 +0000

    Added authentication for adding new campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,16 @@ router.get("/campgrounds", function(req, res){
 });
 
 //CREATE - add new campground to DB
-router.post("/campgrounds", function(req, res){
+router.post("/campgrounds", isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc}
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+    var newCampground = {name: name, image: image, description: desc, author:author}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -33,7 +37,7 @@ router.post("/campgrounds", function(req, res){
 });
 
 //New - shows form to create new campground
-router.get("/campgrounds/new",function(req,res){
+router.get("/campgrounds/new", isLoggedIn, function(req,res){
 	res.render("campgrounds/new");
 });
 
@@ -50,5 +54,12 @@ router.get("/campgrounds/:id", function(req, res){
         }
     });
 });
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 module.exports = router;
