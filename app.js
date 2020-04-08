591359ac54218e132ed8c75ea268f55f6640a42f
Pranay Malyala <malyala.pranay@gmail.com>
2020-04-08 17:40:08 +0000

    Campground edit update and destroy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose    = require("mongoose");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
 var User = require("./models/user");
@@ -22,6 +23,7 @@ mongoose.connect("mongodb://localhost/yelp_camp",{
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 //app.use(flash());
 
 //seedDB(); //seed the database
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -55,6 +55,41 @@ router.get("/campgrounds/:id", function(req, res){
     });
 });
 
+//EDIT CAMPGROUND ROUTE
+router.get("/campgrounds/:id/edit",function(req,res){
+	Campground.findById(req.params.id,function(err,foundCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		}else{
+			res.render("campgrounds/edit",{campground:foundCampground});
+		}
+	});
+});
+//UPDATE CAMPGROUND ROUTE
+router.put("/campgrounds/:id",function(req,res){
+	//find and update the campgrounds
+	Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		}else{
+			res.redirect("/campgrounds/"+req.params.id);
+		}
+	});
+	//redirect somewhere(show page)
+});
+
+//DESTROY CAMPGROUND ROUTE
+router.delete("/campgrounds/:id",function(req,res){
+	Campground.findByIdAndRemove(req.params.id,function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+//middleware
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
