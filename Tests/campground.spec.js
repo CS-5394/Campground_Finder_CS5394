b576a9c695631def0ba50d0500239c2d7a61281f
Pratya Anvesh <pratanvesh@gmail.com>
2020-04-27 18:14:46 -0500

    edit Tests/campground.spec.js

--- Tests/campground.spec.js
+++ Tests/campground.spec.js
@@ -1,67 +1,52 @@
-process.env.NODE_ENV = 'test'
-const request = require("supertest");
+process.env.NODE_ENV = 'test';
+const request = require('supertest');
 // we also need our app for the correct routes!
-const app= require("../app");
+const app = require('../app');
 
-describe('Test the root path', () =>{	
-	
-     it ("It should response the GET method", async() => {	  		 
-	request(app)
-	.get('/campgrounds')
-	.set('Accept', 'application/json')
-    .expect('Content-Type', '/json/')
-	.expect(200); 
-   });
-		 
-     test ('Should post the new campground', async() => {			
-		
-		request(app).post("/campgrounds")  		
-	    .set('Accept', 'application/json')
-	    .expect('Content-Type', '/json/')
-        .expect(200);		
-	
-});
-	   test ('Should get the new campground', async() => {			
-		
-		request(app)
-		.get("/campgrounds/new")         
-	    .set('Accept', 'application/json')
-	    .expect('Content-Type', '/json/')
-        .expect(200);		
-	
-});
-	test ('Should get the /campgrounds/:id ', async() => {
-	      request(app)
-        .get('/campgrounds/:id')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', '/json/')
-        .expect(200)
-	
-	 
+describe('Test the root path', () => {
+  it('It should response the GET method', async () => {
+    request(app)
+      .get('/campgrounds')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200);
   });
-	test ('Should get the /campgrounds/:id/edit ', async() => {
-	      request(app)
-        .get('/campgrounds/:id/edit')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', '/json/')
-        .expect(200)
-	
-	 
+
+  test('Should post the new campground', async () => {
+    request(app)
+      .post('/campgrounds')
+      //Removed mock user data for initial tests
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200);
   });
-	
-	test ('Should put the /campgrounds/:id ', async() => {
-	      request(app)
-        .put('/campgrounds/:id')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', '/json/')
-        .expect(200)
-	
-	 
+  test('Should get the new campground', async () => {
+    request(app)
+      .get('/campgrounds/new')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200);
   });
-	
+  test('Should get the /campgrounds/:id ', async () => {
+    request(app)
+      .get('/campgrounds/:id')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200);
+  });
+  test('Should get the /campgrounds/:id/edit ', async () => {
+    request(app)
+      .get('/campgrounds/:id/edit')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200);
+  });
+
+  test('Should put the /campgrounds/:id ', async () => {
+    request(app)
+      .put('/campgrounds/:id')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .expect(200);
+  });
 });
-	
-	
-	
-	
-	
